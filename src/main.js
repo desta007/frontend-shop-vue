868ee602f5e289b9795015a3e2a78bb5c88584ac
Desta <detajuventini@gmail.com>
2024-09-23 22:45:42 +0700

    first commit frontend shop

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//import router
+import router from './router'
+
+const app = createApp(App)
+
+//gunakan router di vue js dengan plugin "use"
+app.use(router)
+
+//define mixins for global function
+app.mixin({
+
+    methods: {
+
+        //money thousands
+        moneyFormat(number) {
+            let val = (number / 1).toFixed(2).replace('.', ',')
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+
+        },
+
+        //calculate discount
+        calculateDiscount(product) {
+            return product.price - (product.price * (product.discount) / 100)
+        }
+
+    }
+})
+
+app.mount('#app')
